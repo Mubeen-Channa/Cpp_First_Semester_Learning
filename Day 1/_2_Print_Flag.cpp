1c0677fd0aa6a953a41307ff8aee1f6a57846877
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-11-11 00:26:24 +0500

    Day 1 Learning of C++
    Print A Flag

---
+++ Day 1/_2_Print_Flag.cpp
@@ -0,0 +1,18 @@
+// Day 1
+
+#include <iostream>
+using namespace std; 
+
+int main ()
+{
+    // we are printing here american flag 
+    
+    cout<<"THIS IS AN AMERICAN FLAG";
+    cout<<"\n xxxxxxx _ _ _ _ _";
+    cout<<"\n xxxxxxx _ _ _ _ _";
+    cout<<"\n xxxxxxx _ _ _ _ _";
+    cout<<"\n _ _ _ _ _ _ _ _ _";
+    cout<<"\n _ _ _ _ _ _ _ _ _";
+    cout<<"\n _ _ _ _ _ _ _ _ _";
+    cout<<"\n _ _ _ _ _ _ _ _ _";
+}
