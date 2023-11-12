b1b1a2c72d7083b56956821d4ac47fd20bf18ea2
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-11-12 14:22:41 +0500

    Day 1 Learning of C++

---
+++ Day 1/_3_Print_END_Word.cpp
@@ -0,0 +1,15 @@
+// Day 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   	cout<<"\n ||||||||";   
+   	cout<<"\n |          ||     |              ";
+   	cout<<"\n |          | |    |   ||||      ";
+   	cout<<"\n ||||||||   |  |   |   |   |    ";   
+   	cout<<"\n |          |   |  |   |    |   ";
+   	cout<<"\n |          |    | |   |   |     ";
+   	cout<<"\n ||||||||   |     ||   ||||       ";
+}
