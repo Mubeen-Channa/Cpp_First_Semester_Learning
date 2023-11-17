09cf98aeb2d80c1b92bf63c64d5e23469592bf28
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-11-17 18:22:00 +0500

    Day 1 Learning of C++

---
+++ Day 1/_4_Print_C++_with_characters.cpp
@@ -0,0 +1,14 @@
+// Day 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"\n ccccccccc      ++              ++               ";
+    cout<<"\n cc             ++              ++               ";
+    cout<<"\n cc        ++++++++++++    ++++++++++++          ";
+    cout<<"\n cc        ++++++++++++    ++++++++++++          ";
+    cout<<"\n cc             ++              ++               ";
+    cout<<"\n ccccccccc      ++              ++               " <<endl;
+}
