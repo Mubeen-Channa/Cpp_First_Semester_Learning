1f7dc9713365d18ba8f072173d4d28170d021955
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-11-26 17:14:46 +0500

    _8_Comments.cpp

--- Day 2/_8_Comments.cpp
+++ Day 2/_8_Comments.cpp
@@ -1,8 +1,21 @@
-// Day 1
+// Day 2
 
 #include <iostream>
 using namespace std;
 
 int main (){
     
+    // Single-line comment: This is a single-line comment
+
+    /*
+       Multi-line comment: This is a multi-line comment
+       It can span multiple lines
+    */
+
+   // Comments used for multiple reasons like as Code Explanation, Documentation, Code Readbility
+
+    int a=10;
+    int b=20;
+
+    int x=a+b;     // x store sum of variable 'a' and 'b'
 }
