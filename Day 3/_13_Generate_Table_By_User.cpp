52dd68733898ad780669c78c37b366f7811f652f
Muhammad Mubeen Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2024-04-11 18:23:43 +0500

    Day 3 Learning of C++

---
+++ Day 3/_13_Generate_Table_By_User.cpp
@@ -0,0 +1,22 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int num;
+   	cout<<"ENTER TABLE NUMBER : "; cin>>num;    
+   	cout<<endl;
+
+   	cout<<a<<" *  1  = "<<a*1 <<endl;
+   	cout<<a<<" *  2  = "<<a*2 <<endl;
+   	cout<<a<<" *  3  = "<<a*3 <<endl;
+   	cout<<a<<" *  4  = "<<a*4 <<endl;
+   	cout<<a<<" *  5  = "<<a*5 <<endl;
+   	cout<<a<<" *  6  = "<<a*6 <<endl;
+   	cout<<a<<" *  7  = "<<a*7 <<endl;
+   	cout<<a<<" *  8  = "<<a*8 <<endl;
+   	cout<<a<<" *  9  = "<<a*9 <<endl;
+   	cout<<a<<" *  10 = "<<a*10 <<endl;     
+}
