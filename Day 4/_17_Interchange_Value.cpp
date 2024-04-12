12f5d7a6c37e623475756087c98b49633863c8f9
Muhammad Mubeen Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2024-04-12 13:30:50 +0500

    Day 4 Learning of C++

---
+++ Day 4/_17_Interchange_Value.cpp
@@ -0,0 +1,16 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<< " a = 10 and b = 70"<<endl;
+	int a = 10;
+	int b = 70;
+	
+	a=a+b ; 
+	b=a-b ;
+	a=a-b ;
+	
+	cout<<"\n a = "<<a<<endl<<" b = "<<b;  
+}
