2ad6458be34652ff1c1dc2c36d7465d2d35d852b
Muhammad Mubeen Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2024-04-13 17:12:17 +0500

    Day 5 Learning of C++

---
+++ Day 5/_21_Break.cpp
@@ -0,0 +1,15 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    // Break Keyword is used for jump out from switch and loop   
+    
+    for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+            break;
+        }
+        cout << i <<"\n";
+    }
+}
