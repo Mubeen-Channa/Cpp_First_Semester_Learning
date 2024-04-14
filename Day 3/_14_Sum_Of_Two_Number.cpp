918ad110c6bb6e29be8885f15b2aac5cc0b7d9ad
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-04-14 19:52:49 +0500

    Day 3 Learning of C++

---
+++ Day 3/_14_Sum_Of_Two_Number.cpp
@@ -0,0 +1,19 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int x, y;
+    int sum;
+
+    cout << "Type a number: ";
+    cin >> x;
+    
+    cout << "Type another number: ";
+    cin >> y;
+    
+    sum = x + y;
+    cout << "Sum is: " << sum;
+}
