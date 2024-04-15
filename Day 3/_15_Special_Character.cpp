e15bde11a52197271017ca75f043665996699ef7
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-04-15 07:00:11 +0500

    Day 3 Learning of C++

---
+++ Day 3/_15_Special_Character.cpp
@@ -0,0 +1,30 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    /* 1. we cant use # in beganing of variable name
+	   int #Pakistan = 10;
+   	cout<< #Pakistan; */
+   	
+   	/* 2. we cant use spacial charaters !,@,#,$,%,^,&,* in variable names
+	   int Pak*istan = 10;
+   	cout<< Pak*istan; */
+   	
+   	/* 3. we cant use space in variable name
+	   int Pak istan = 10;
+   	cout<< Pak istan; */
+   	
+   	/* 4. we cant use number in start of variable name
+	   int 12Pakistan = 10;
+   	cout<< 12Pakistan; */
+   	
+   	// 5. we can use number in start of variable name with help of underscore
+	   int _12Pakistan = 10;
+   	cout<< _12Pakistan <<endl;
+   	
+   	// 6. we cant use space in variable name, if we want we use undersore instead of space
+	   int Pak_istan = 10;
+   	cout<< Pak_istan;
+}
