9b698d6d101175fce54f85b6d7cd799b8088ea2c
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-04-16 10:15:15 +0500

    Day 3 Learning of C++

---
+++ Day 3/_16_Square_&_Cube.cpp
@@ -0,0 +1,18 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	   
+	cout<<" \n Enter a number for square : "<<endl;
+	int s;
+	cin>>s;
+	cout<<"Square of "<<s<<" is "<<s*s<<endl;
+	   
+	cout<<" \n Enter number for cube"<<endl;
+	int c;
+	cin>>c;
+	cout<<"Cube of "<<c<<" is "<<c*c*c<<endl;  
+   
+}
