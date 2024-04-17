4d2f9ec0ccb214d9e0e75c4cbbb4344e63558ebf
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-04-17 07:31:16 +0500

    Day 4 Learning of C++

---
+++ Day 4/_18_String_Concatenation.cpp
@@ -0,0 +1,16 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    /*
+        The + operator can be used between strings to add them together to make a new string
+        This is called concatenation
+    */
+
+    string firstName = "Muhammad ";
+    string lastName  = "Mubeen";
+    string fullName  = firstName + lastName;
+    cout <<"Your Full Name Is : "<<fullName;  
+}
