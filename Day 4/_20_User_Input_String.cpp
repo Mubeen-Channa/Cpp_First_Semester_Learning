1bf5ea03a546d70627e98a10ab642f88e2a1999a
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-04-18 14:56:20 +0500

    Day 4 Learning of C++

---
+++ Day 4/_20_User_Input_String.cpp
@@ -0,0 +1,18 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+    string firstName;
+    string lastName;
+
+    cout << "Enter your first name : ";
+    cin >> firstName;
+
+    cout << "Enter your second name : ";
+    cin >> lastName;
+
+    cout << "Your name is: " << firstName+lastName;
+}
